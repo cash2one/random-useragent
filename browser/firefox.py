710baaf4971b56b76cd17a5407ebb5680b45b5ef
jlfme <jlfgeek@gmail.com>
2017-03-07 03:25:26 +0800

    Add linux versions

--- browser/firefox.py
+++ browser/firefox.py
@@ -16,6 +16,8 @@ import random
 WINDOWS_VERSIONS = [5.1, 5.2, 6.0, 6.1, 6.2, 6.3, 10.0]
 MAC_VERSIONS = [10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 10.10, 10.11, 10.12, 10.13]
 ANDROID_VERSIONS = [4.0, 4.1, 4.3, 4.4, 5.0, 5.1, 6.0, 7.0, 7.1, 8.0]
+LINUX_VERSIONS = ['Ubuntu', 'U']
+
 FIREFOX_VERSIONS = [float(i) for i in range(41, 57)]
 
 
@@ -33,6 +35,9 @@ PLATFORMS_MAP = {
         'X11; Linux i686',
         'X11; Linux x86_64',
         'X11; Linux i686 on x86_64',
+        'X11; {version}; Linux i686',
+        'X11; {version}; Linux x86_64',
+        'X11; {version}; Linux i686 on x86_64',
     ],
     'android': [
         'Android {version}; Mobile',
@@ -44,7 +49,8 @@ PLATFORMS_MAP = {
 PLATFORM_VERSIONS_MAP = {
     'windows': WINDOWS_VERSIONS,
     'mac': MAC_VERSIONS,
-    'android': ANDROID_VERSIONS
+    'android': ANDROID_VERSIONS,
+    'linux': LINUX_VERSIONS
 }
 
 
